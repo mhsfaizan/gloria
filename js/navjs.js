b8e58eba19ce7864110aed9ee0319c49167d6349
aeshar <aeshar@gmail.com>
2018-07-24 14:34:51 +0530

    home search

--- js/navjs.js
+++ js/navjs.js
@@ -15,6 +15,7 @@ $(document).ready(function() {
 	});
 
 	$('.search-link .fa').click(function() {
+		
 		if ($('.search-link .input-box input').hasClass('hide-input')) {
 			$('.search-link .input-box input').addClass('show-input').removeClass('hide-input');
 			$('.search-link .fa').addClass('fa-times').removeClass('fa-search');
