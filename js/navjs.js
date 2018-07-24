faf8a70ac7c329a96f0b333f5e43ffe31dee7323
aeshar <aeshar@gmail.com>
2018-07-24 14:30:53 +0530

    homepage search

--- js/navjs.js
+++ js/navjs.js
@@ -2,15 +2,27 @@ $(document).ready(function() {
 	$('.collapse-btn').click(()=>{
 		$('.sidebar-bg').show();
 		$('.sidebar').css('left','0');
-	})
+	});
 
 	$('.sidebar-bg').click(()=>{
 		$('.sidebar-bg').hide();
 		$('.sidebar').css('left','-260px');
-	})
+	});
 
 	$('.close-btn-div').click(()=>{
 		$('.sidebar-bg').hide();
 		$('.sidebar').css('left','-260px');	
-	})
+	});
+
+	$('.search-link .fa').click(function() {
+		if ($('.search-link .input-box input').hasClass('hide-input')) {
+			$('.search-link .input-box input').addClass('show-input').removeClass('hide-input');
+			$('.search-link .fa').addClass('fa-times').removeClass('fa-search');
+			$('.whole-page-layer').show();
+		}else{
+			$('.search-link .input-box input').addClass('hide-input').removeClass('show-input');
+			$('.search-link .fa').addClass('fa-search').removeClass('fa-times');
+			$('.whole-page-layer').hide();
+		}
+	});
 });
