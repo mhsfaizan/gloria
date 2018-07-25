43ecbf65153044efd0044860bcce159aa189b025
faizan <faizan@gmail.com>
2018-07-25 13:00:38 +0530

    todays commit

--- js/navjs.js
+++ js/navjs.js
@@ -25,4 +25,7 @@ $(document).ready(function() {
 			$('.whole-page-layer').hide();
 		}
 	});
+	$(".sidebar-links a:nth-child(2)").click(()=>{
+		$(".mydropdown-menu").toggleClass("mydropdown-menu-show");
+	});
 });
