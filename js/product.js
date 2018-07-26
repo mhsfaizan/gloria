e62b65311d14b394a97f68a9f4d9a0f1c69db9d2
aeshar <aeshar@gmail.com>
2018-07-26 14:25:01 +0530

    Prodct Commit

--- js/product.js
+++ js/product.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-	$('.card-header').click((event)=>{
-		if ($(event.target).find('.symbol').hasClass('fa-minus')) {
-			$(event.target).find('.symbol').removeClass('fa-minus').addClass('fa-plus');
-		}else{
-			$(event.target).find('.symbol').removeClass('fa-plus').addClass('fa-minus');
-		}
-		$(event.target).next().toggle('100');
-	});
+	
+	// $('.card-header').click((event)=>{
+	// 	if ($(event.target).find('.symbol').hasClass('fa-minus')) {
+	// 		$(event.target).find('.symbol').removeClass('fa-minus').addClass('fa-plus');
+	// 	}else{
+	// 		$(event.target).find('.symbol').removeClass('fa-plus').addClass('fa-minus');
+	// 	}
+	// 	$(event.target).next().toggle('100');
+	// });
 
 	$(".owl-carousel").owlCarousel({
 		nav : true,
