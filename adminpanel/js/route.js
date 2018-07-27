68aee3d5ffc8850dbd06a2de90c2724e7b10b779
faizan <faizan@gmail.com>
2018-07-27 18:52:33 +0530

    admin commited

---
+++ adminpanel/js/route.js
@@ -0,0 +1,29 @@
+app.config(($routeProvider,$locationProvider)=>{
+	var myresolve = ()=>{
+		return {
+			"check":($location,saveData)=>{
+				if(!saveData.isLoggedIn()){
+					$location.path("./");
+				}
+			}
+		}
+	}
+	$routeProvider
+	.when("/",{
+		templateUrl:"pages/login.php"
+	})
+	
+	.when("/logout",{
+		resolve:{
+			deadresolve:(saveData,$location)=>{
+				saveData.clearData();
+				$location.path("./");
+			}
+		}
+	})
+	.otherwise({
+		templateUrl:"pages/login.php"
+	})
+	$locationProvider.html5Mode(true);
+	
+});
